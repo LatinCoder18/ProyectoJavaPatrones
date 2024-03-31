e2b7e658b0e7f330e942c20a7985a9c2beea1adb
AFVA97 <165512616+AFVA97@users.noreply.github.com>
2024-03-30 23:33:18 -0400

    definicion de metodos a implementar por la interfaz QuickSearch

--- src/Browser/QuickSearch.java
+++ src/Browser/QuickSearch.java
@@ -2,4 +2,6 @@ package Browser;
 
 public interface QuickSearch {
     //Esta interfaz podrá ser utilizada para implementar otros de los patrones.
+    public void searchWord(String param);
+    public void searchImage(String name);
 }
